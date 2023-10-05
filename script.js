d2c34c634b2aaab356276b2e99b797d9c6a5e3f1
GrzLudo <ludovic.grzelczak@hotmail.com>
2023-10-05 16:17:12 +0200

    update V1.01 / adding CV-pdf download

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const headerTitle = document.querySelector(".typewriter");
 const txt = "Développeur Web Junior";
 const myName = document.querySelector(".header-container");
+const nav = document.querySelector("nav");
+const boxNavHome = document.querySelector(".navbox-1");
+const boxNavAboutMe = document.querySelector(".navbox-2");
+const boxNavResume = document.querySelector(".navbox-3");
+const boxNavCareer = document.querySelector(".navbox-4");
+const boxNavContact = document.querySelector(".navbox-6");
 
 // ************************ animation header-text *************
 function typewriter(word, index) {
@@ -17,24 +23,40 @@ setTimeout(() => {
 
 // ************************ animation header-container *************
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 70) {
-    myName.classList.add("animate__animated", "animate__fadeOutDown");
-  } else {
-    myName.classList.remove("animate__animated", "animate__fadeOutDown");
-    myName.classList.add("animate__animated", "animate__fadeInUp");
-  }
-});
-
-const nav = document.querySelector("nav");
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 10) {
+//     myName.classList.add("animate__animated", "animate__fadeOutDown");
+//   } else {
+//     myName.classList.remove("animate__animated", "animate__fadeOutDown");
+//     myName.classList.add("animate__animated", "animate__fadeInUp");
+//   }
+// });
 
 window.addEventListener("scroll", () => {
   console.log(window.scrollY);
-  if (window.scrollY > 800) {
+  if (window.scrollY > 800 && window.scrollY <= 1650) {
     nav.style.background = "rgb(235, 235, 235)";
     nav.style.top = 0;
-  } else {
+  } else if (window.scrollY > 1650 && window.scrollY <= 2500) {
     nav.style.background = "rgba(255, 255, 255, 1)";
+    nav.style.top = 0;
+  } else if (window.scrollY > 2500 && window.scrollY <= 4950) {
+    nav.style.background = "rgba(235, 235, 235)";
+    nav.style.top = 0;
+    boxNavHome.style.color = "rgba(0, 0, 0)";
+    boxNavAboutMe.style.color = "rgba(0, 0, 0)";
+    boxNavResume.style.color = "rgba(0, 0, 0)";
+    boxNavCareer.style.color = "rgba(0, 0, 0)";
+    boxNavContact.style.color = "rgba(0, 0, 0)";
+  } else if (window.scrollY > 4900) {
+    nav.style.background = "rgb(58, 58, 58)";
+    nav.style.top = 0;
+    boxNavHome.style.color = "rgba(255, 255, 255)";
+    boxNavAboutMe.style.color = "rgba(255, 255, 255)";
+    boxNavResume.style.color = "rgba(255, 255, 255)";
+    boxNavCareer.style.color = "rgba(255, 255, 255)";
+    boxNavContact.style.color = "rgba(255, 255, 255)";
+  } else {
     nav.style.top = "-60px";
   }
 });
